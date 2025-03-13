c88d92a221fa6d2cb16e585b3a28113e78565dc7
Hojun Kim <didl95@naver.com>
2025-03-13 15:15:02 +0900

    [Silver I] Title: 카드 구매하기, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,10 @@
+n = int(input())
+cards = list(map(int, input().split()))
+cards.insert(0, 0)
+
+dp = [0] * (n + 1)
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], cards[j] + dp[i - j])
+
+print(dp[n])
