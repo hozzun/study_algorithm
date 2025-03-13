8de91eef74e44cf4082803f25d2463a0ff0c06f0
Hojun Kim <didl95@naver.com>
2025-03-13 23:33:32 +0900

    [level 2] Title: 귤 고르기, Time: 26.25 ms, Memory: 20.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+def solution(k, tangerine):
+    counter = Counter(tangerine)
+    arr = sorted(counter.values(), reverse=True)
+    
+    count = 0
+    kind = 0
+    for gyul in arr:
+        count += gyul
+        kind += 1
+        
+        if count >= k:
+            return kind
