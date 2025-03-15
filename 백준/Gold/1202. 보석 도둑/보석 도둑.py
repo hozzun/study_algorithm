73dddebbcba760d2b5b330dc54ea8a6f321b7fec
Hojun Kim <didl95@naver.com>
2025-03-15 15:51:25 +0900

    [Gold II] Title: 보석 도둑, Time: 1224 ms, Memory: 99460 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -0,0 +1,30 @@
+import heapq, sys
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+gems = []
+for _ in range(n):
+    m, v = map(int, input().split())
+    gems.append((m, v))
+gems.sort()
+
+bags = []
+for _ in range(k):
+    c = int(input())
+    bags.append(c)
+bags.sort()
+
+max_heap = []
+answer = 0
+idx = 0
+
+for bag in bags:
+    while idx < n and gems[idx][0] <= bag:
+        heapq.heappush(max_heap, -gems[idx][1])
+        idx += 1
+
+    if max_heap:
+        answer += -heapq.heappop(max_heap)
+
+print(answer)
