2f151d56fe449182b4d5274425d2e7a8602c4249
Hojun Kim <didl95@naver.com>
2025-03-28 22:30:25 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+def f(x):
+    if x == M:
+        print(*arr)
+        return
+
+    for i in range(1, N+1):
+        if arr and i < arr[-1]:
+            continue
+        arr.append(i)
+        f(x + 1)
+        arr.pop()
+
+N, M = map(int, input().split())
+arr = []
+f(0)
