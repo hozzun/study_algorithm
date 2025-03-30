a07d257442b9d350fc36ef166fa37cc1ab6c1703
Hojun Kim <didl95@naver.com>
2025-03-30 12:17:25 +0900

    [level 2] Title: 주식가격, Time: 24.42 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    n = len(prices)
+    answer = [0] * n
+    stack = []
+    
+    for idx, price in enumerate(prices):
+        while stack and prices[stack[-1]] > price:
+            stack_idx = stack.pop()
+            answer[stack_idx] = idx - stack_idx
+        stack.append(idx)
+    
+    while stack:
+        stack_idx = stack.pop()
+        answer[stack_idx] = n - 1 - stack_idx
+        
+    return answer
