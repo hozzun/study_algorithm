5e19dd26c1621c5718128ebfd485a79259152807
Hojun Kim <didl95@naver.com>
2025-04-15 23:26:48 +0900

    [Silver V] Title: 막대기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -0,0 +1,14 @@
+X = int(input())
+stick = [64, 32, 16, 8, 4, 2, 1]
+
+count = 0
+for i in range(len(stick)):
+    if X >= stick[i]:
+        count += 1
+        X -= stick[i]
+        if X == 0:
+            break
+    else:
+        pass
+
+print(count)
