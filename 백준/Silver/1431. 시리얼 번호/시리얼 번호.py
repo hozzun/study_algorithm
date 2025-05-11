2d0c7c85e52a6948c9e17c73f6a1fc666cfe5a17
Hojun Kim <didl95@naver.com>
2025-05-11 12:55:38 +0900

    [Silver III] Title: 시리얼 번호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+serial_numbers = []
+for _ in range(n):
+    serial_numbers.append(input())
+
+serial_numbers.sort(key=lambda x: (len(x), sum(int(num) for num in x if num.isdigit()), x))
+
+for number in serial_numbers:
+    print(number)
